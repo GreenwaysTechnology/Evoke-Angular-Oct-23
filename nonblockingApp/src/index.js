82558396961e8aa1de8781ad05a689f77e89127e
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-10-25 21:53:40 +0530

    rxjs

--- nonblockingApp/src/index.js
+++ nonblockingApp/src/index.js
@@ -1,63 +1,19 @@
-//compostion: the out put of one callback will be input to another callback. 
 
-function getUser() {
-    console.log('getUser is called')
-    return new Promise((resolve, reject) => {
-        let user = {
-            name: 'admin', password: 'admin'
-        }
-        // user=null
-        if (user) {
-            setTimeout(resolve, 1000, user)
-        } else {
-            setTimeout(reject, 1000, 'User not found')
-        }
-    })
-}
-function login(user) {
-    console.log('login is called')
-    return new Promise((resolve, reject) => {
-        if (user.name === 'admin' && user.password === 'admin') {
-            setTimeout(resolve, 1000, 'login success')
-        } else {
-            setTimeout(reject, 1000, 'login failed')
-        }
-    })
+import { Observable } from 'rxjs'
 
-}
-function showDashboard(status) {
-    console.log('showDashboard is called')
-    return new Promise((resolve, reject) => {
-        if (status === 'login success') {
-            setTimeout(resolve, 1000, 'welcome to admin')
-        } else {
-            setTimeout(reject, 1000, 'Welcome to guest')
-        }
-    })
+//create stream from scratch
 
-}
+//publisher
+const observable = new Observable(subscriber => {
+    subscriber.next(1) //emit data event
+    subscriber.next(2) //emit data event
+    subscriber.next(3) //emit data event
+    subscriber.next(4) //emit data event
+})
 
-async function main() {
-    // getUser()
-    //     .then(user => {
-    //         return login(user)
-    //     })
-    //     .then(status => {
-    //         return showDashboard(status)
-    //     }).then(page => {
-    //         console.log(page)
-    //     })
-    //     .catch(err => console.log(err))
-
-    try {
-        let user = await getUser()
-        let status = await login(user)
-        const page = await showDashboard(status)
-        console.log(page)
-    } catch (err) {
-        console.log(err)
+//subscribers
+observable.subscribe({
+    next(data) {
+        console.log(`got value ${data}`)
     }
-
-
-}
-main()
+})
